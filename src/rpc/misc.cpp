47165988e1778ac532a5bf8a9a63442a334d56e1
kiiglobal <jcc@kiire.io>
2022-09-26 16:40:42 -0400

    change wrongly mentioned versions correctly

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -199,7 +199,7 @@ UniValue validateaddress(const JSONRPCRequest& request)
             "validateaddress \"address\"\n"
             "\nReturn information about the given kii address.\n"
             "DEPRECATION WARNING: Parts of this command have been deprecated and moved to getaddressinfo. Clients must\n"
-            "transition to using getaddressinfo to access this information before upgrading to v0.18. The following deprecated\n"
+            "transition to using getaddressinfo to access this information before upgrading to v1.0.2. The following deprecated\n"
             "fields have moved to getaddressinfo and will only be shown here with -deprecatedrpc=validateaddress: ismine, iswatchonly,\n"
             "script, hex, pubkeys, sigsrequired, pubkey, addresses, embedded, iscompressed, account, timestamp, hdkeypath.\n"
             "\nArguments:\n"
@@ -285,7 +285,7 @@ UniValue createmultisig(const JSONRPCRequest& request)
         if (IsHex(keys[i].get_str()) && (keys[i].get_str().length() == 66 || keys[i].get_str().length() == 130)) {
             pubkeys.push_back(HexToPubKey(keys[i].get_str()));
         } else {
-            throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, strprintf("Invalid public key: %s\nNote that from v0.16, createmultisig no longer accepts addresses."
+            throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, strprintf("Invalid public key: %s\nNote that from v1.0.1, createmultisig no longer accepts addresses."
             " Users must use addmultisigaddress to create multisig addresses with addresses known to the wallet.", keys[i].get_str()));
         }
     }
