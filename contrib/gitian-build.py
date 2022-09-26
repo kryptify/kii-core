2fb5fa87bf9955cf389f6525a73489aa69bb4966
kiiglobal <jcc@kiire.io>
2022-09-26 03:03:10 -0400

    change github link in contrib/gitian-build.py

--- contrib/gitian-build.py
+++ contrib/gitian-build.py
@@ -29,7 +29,7 @@ def setup():
     if not os.path.isdir('gitian-builder'):
         subprocess.check_call(['git', 'clone', 'https://github.com/devrandom/gitian-builder.git'])
     if not os.path.isdir('kii'):
-        subprocess.check_call(['git', 'clone', 'https://github.com/kiiglobal/kii.git'])
+        subprocess.check_call(['git', 'clone', 'https://github.com/kiiglobal/kii-core.git'])
     os.chdir('gitian-builder')
     make_image_prog = ['bin/make-base-vm', '--suite', 'bionic', '--arch', 'amd64']
     if args.docker:
