3a8c0e1dea1b13e38bdb55a6245457be7fd5c2fd
root <root@usdt.kii.global>
2022-10-20 22:22:37 -0400

    RC1 for v1.0.1.2

---
+++ contrib/install.sh
@@ -0,0 +1,19 @@
+#para compilar kii para arm
+#soporte para arm
+
+
+sudo apt-get \
+     install \
+     curl \
+     g++-aarch64-linux-gnu \
+     g++-9-aarch64-linux-gnu \
+     gcc-9-aarch64-linux-gnu \
+     binutils-aarch64-linux-gnu \
+     g++-arm-linux-gnueabihf \
+     g++-9-arm-linux-gnueabihf \
+     gcc-9-arm-linux-gnueabihf \
+     binutils-arm-linux-gnueabihf \
+     g++-9-multilib \
+     gcc-9-multilib \
+     binutils-gold \
+     bsdmainutils
